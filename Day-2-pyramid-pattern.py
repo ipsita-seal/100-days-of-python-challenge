6d84ece74e9b622826bd10cf8e701c1b7a086d2a
Ipsita Seal <sealipsita15@gmail.com>
2026-06-09 06:18:06 +0100

    Create Day-2-pyramid-pattern.py

---
+++ Day-2-pyramid-pattern.py
@@ -0,0 +1,8 @@
+#Pyramid Pattern
+print("3. Pyramid pattern")
+for i in range(5):
+    for j in range(5 - i - 1):
+        print(" ", end="")
+    for k in range(2 * i + 1):
+        print("*", end="")
+    print()
