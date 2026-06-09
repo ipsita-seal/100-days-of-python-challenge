c00fc34ea684064415ccd07b151237790317ab58
Ipsita Seal <sealipsita15@gmail.com>
2026-06-09 05:56:01 +0100

    Add patterns for right triangle, inverted triangle, and pyramid

---
+++ Day-2_Patterns.py
@@ -0,0 +1,24 @@
+#1. right triangle pattern
+print("1. right triangle pattern")
+for i in range(5):
+    for j in range(i + 1):
+        print("*", end="")
+    print()
+print("\n")
+
+#2. Inverted Triangle Pattern
+print("2. Inverted Triangle pattern")
+for i in range(5, 0, -1):
+    for j in range(i):
+        print("*", end="")
+    print()
+print("\n")
+
+#3. Pyramid Pattern
+print("3. Pyramid pattern")
+for i in range(5):
+    for j in range(5 - i - 1):
+        print(" ", end="")
+    for k in range(2 * i + 1):
+        print("*", end="")
+    print()
