53dcdce553a8d18f37f4a12325f5771dc472b197
Ipsita Seal <sealipsita15@gmail.com>
2026-06-13 13:01:15 +0100

    Fix bubble sort loop to iterate correctly

--- Day-6-bubble-sort.py
+++ Day-6-bubble-sort.py
@@ -1,6 +1,6 @@
 #Bubble sort
 l1=[9,8,7,6,5,4,3,2,1,0]
-for i in range(1,len(l1)):
+for i in range(len(l1)):
     for i in range(len(l1)-1):
         if l1[i]>l1[i+1]:
             l1[i],l1[i+1]=l1[i+1],l1[i]
